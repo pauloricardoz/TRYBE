d9ab5e102cd9325bde7063c6a51dbabe8050fb45
pauloricardoz <trybe.przt@gmail.com>
2020-05-14 18:42:14 -0300

    exercise 1 2

---
+++ Block_4/4-3/ex1.js
@@ -0,0 +1,11 @@
+let n=5;
+let text=""
+for (let i = 0; i < n; i++) {
+  for (let j = 0; j < n; j++) {
+    text+="*"
+    
+  };
+  console.log(text);
+  text=""
+  
+}
---
+++ Block_4/4-3/ex2.js
@@ -0,0 +1,7 @@
+let n = 5;
+let text = ""
+for (let i = 0; i <=n; i++) {
+ 
+  console.log(text);
+  text += "*"
+}

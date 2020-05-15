495ebe3d2c36db31ec6731e0070136b1d6a6be44
pauloricardoz <trybe.przt@gmail.com>
2020-05-15 08:30:01 -0300

    corrections to ex4 to accept even numbers

--- Block_4/4-3/ex4.js
+++ Block_4/4-3/ex4.js
@@ -1,18 +1,26 @@
-let n = 5;
+let n = 9;
 let text = "     "
 let v = [];
 for (let index = 0; index < n; index++) {
   v.push(" ");
 
 }
+let IsEven = false;
+if (n % 2 == 0)
+  IsEven = true;
 
-let inicio = Math.trunc(n / 2);
-v[inicio] = "*";
-console.log(v.join(" "));
-for (let i = 1; i <= n; i++) {
+  let inicio = Math.trunc(n / 2); //pega o n, divide por 2 para descobrir o meio do vetor e remove as casas decimais
+
+for (let i = 0; i <= n; i++) {
   if (v[0] == "*")
     break;
-  v[inicio - i] = "*";
-  v[inicio + i] = "*";
+  if (IsEven == false) {
+
+    v[inicio - i] = "*";
+    v[inicio + i] = "*";
+  } else {
+    v[inicio - i] = "*";
+    v[inicio +1+ i] = "*";
+  }
   console.log(v.join(" "));
 }

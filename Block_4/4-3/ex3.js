88c065cacba0ea6bd4b2b8dccc3baa0d90f8790d
pauloricardoz <trybe.przt@gmail.com>
2020-06-01 17:33:11 -0300

    updating

--- Block_4/4-3/ex3.js
+++ Block_4/4-3/ex3.js
@@ -1,13 +1,12 @@
 let n = 5;
 let vetor = new Array(n);   //cria um array com um tamanho n
-vetor.fill(" ", 0, n)       //Preenche o array com espaços vazios // essa linha e a debaixo pode ser substituido por um "for"
+vetor.fill(" ")       //Preenche o array com espaços vazios // essa linha e a debaixo pode ser substituido por um "for"
 let text = vetor.join("");  //Passa o array para uma string
 
 
 for (let i = 0; i <= n; i++) {
 
   console.log(text);
-
   text += "*"
   text = text.substr(1);
 }

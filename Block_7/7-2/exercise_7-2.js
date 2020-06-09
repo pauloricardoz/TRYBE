8a18c43f9b161eced5baf03aeb7dd0d82e5e7458
pauloricardoz <trybe.przt@gmail.com>
2020-06-09 13:31:19 -0300

    ex1

---
+++ Block_7/7-2/exercise_7-2.js
@@ -0,0 +1,27 @@
+const lesson1 = {
+  materia: 'Matemática',
+  numeroEstudantes: 20,
+  professor: 'Maria Clara',
+  turno: 'manhã',
+};
+
+const lesson2 = {
+  materia: 'História',
+  numeroEstudantes: 20,
+  professor: 'Carlos',
+};
+
+const lesson3 = {
+  materia: 'Matemática',
+  numeroEstudantes: 10,
+  professor: 'Maria Clara',
+  turno: 'noite',
+};
+
+function turno(objeto, chave, valor) {
+  objeto[chave] = valor;
+}
+
+console.log(lesson2);
+turno(lesson2, 'turno', 'manha');
+console.log(lesson2);

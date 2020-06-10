3d8c6c9001025cea075f05ae19eb7ddf52e00268
pauloricardoz <trybe.przt@gmail.com>
2020-06-10 16:35:45 -0300

    desafio 2 1

--- Block_7/desafio2.js
+++ Block_7/desafio2.js
@@ -1,10 +1,14 @@
 const assert = require('assert');
 // escreva a função wordLengths para passar nos testes abaixo:
-
+const wordLengths = () => {};
 //
 const words = ['sun', 'potato', 'roundabout', 'pizza'];
 const expected = [3, 6, 10, 5];
 
-assert.strictEqual(typeof wordLengths, 'function');
+assert.strictEqual(
+  typeof wordLengths,
+  'function',
+  `erro1 ${typeof wordLengths}`
+);
 const output = wordLengths(words);
-assert.deepEqual(output, expected);
+assert.deepEqual(output, expected, `erro2 ${output} ${output}`);

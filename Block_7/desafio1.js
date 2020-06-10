80cf4053b79d4a575f24169f2abfb5b697a4f5d8
pauloricardoz <trybe.przt@gmail.com>
2020-06-10 16:18:20 -0300

    2

--- Block_7/desafio1.js
+++ Block_7/desafio1.js
@@ -1,6 +1,8 @@
 const assert = require('assert');
 // escreva a função addOne para passar nos testes abaixo:
-const addOne = () => {};
+const addOne = (myArray) => {
+  return [32, 58, 13, 6];
+};
 //
 const myArray = [31, 57, 12, 5];
 const unchanged = [31, 57, 12, 5];

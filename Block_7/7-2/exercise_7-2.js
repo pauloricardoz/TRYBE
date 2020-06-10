f51214cb369097ddd7346a833b94cdbb228c64d2
pauloricardoz <trybe.przt@gmail.com>
2020-06-10 15:53:15 -0300

    setup

--- Block_7/7-2/exercise_7-2.js
+++ Block_7/7-2/exercise_7-2.js
@@ -21,14 +21,16 @@ const lesson3 = {
 function turno(objeto, chave, valor) {
   objeto[chave] = valor;
 }
-
 function listaChaves(objeto) {
   console.log(Object.keys(objeto));
 }
-
 function tamanho(objeto) {
   console.log(Object.keys(objeto).length);
 }
-
-console.log(lesson2);
-tamanho(lesson2);
+function valores(objeto) {
+  const test = Object.entries(objeto);
+  console.log(test);
+}
+const allLessons = Object.assign({}, { lesson1 }, { lesson2 }, { lesson3 });
+console.log(allLessons);
+const a=requere('assest')
